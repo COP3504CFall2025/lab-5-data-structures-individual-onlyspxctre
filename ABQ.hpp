c4de443717b2541f593b202fc3b8bbe5b1aa74cf
onlyspxctre <onlyspxctre@proton.me>
2025-11-07 16:33:30 -0500

    Dealloc when shrun

--- ABQ.hpp
+++ ABQ.hpp
@@ -140,6 +140,10 @@ public:
             throw std::runtime_error("ABQ is empty");
         }
         this->curr_size_ -= 1;
+
+        if (this->curr_size_ <= this->capacity_ / 4) {
+            realloc(this->capacity_ >> 1);
+        }
         return this->array_[this->index_ - this->curr_size_ - 1];
     }
 
